8a3dedd583e20f847ce1ff6b339629f634486d3d
iamqjin <iamqjin@gmail.com>
2017-02-06 16:44:14 +0900

    Initial Commit

---
+++ HealthScheduler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthScheduler
+//
+//  Created by iamqjin on 2017. 2. 6..
+//  Copyright © 2017년 iamqjin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
